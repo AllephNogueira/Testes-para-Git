6d280378fb0daa590d018836a8f7fdfe9c475d88
Alleph Nogueira <allephn@hotmail.com.br>
2024-03-14 21:42:21 -0300

    Trocando o texto para a versao de 13.0 que estava errada!

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 document.querySelector('h1').innerHTML = "Olá, mundo!!!";
-document.querySelector('p').innerHTML = 'Esse é minha versao 12.0';
+document.querySelector('p').innerHTML = 'Esse é minha versao 13.0';
 document.querySelector('.segundo paragrafo') = 'Esse é meu segundo 5';
