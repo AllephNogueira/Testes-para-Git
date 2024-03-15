8e9b850eea91f10f276714bc3d17cc4b104d8a90
Alleph Nogueira <allephn@hotmail.com.br>
2024-03-14 21:26:04 -0300

    Revert "Nova atualização."
    This reverts commit 074dfa15afbdc917f4ba14c86e935a895e342f76.

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 document.querySelector('h1').innerHTML = "Olá, mundo!!!";
-document.querySelector('p').innerHTML = 'Esse é minha versao 11.0';
+document.querySelector('p').innerHTML = 'Esse é minha versao 9.0';
 document.querySelector('.segundo paragrafo') = 'Esse é meu segundo 5';
